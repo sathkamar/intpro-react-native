0453b3232328fb5fdb9982ce3ff6e82edd462779
Kamaraj <skamaraj@in.imshealth.com>
2021-08-10 23:30:11 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+// In App.js in a new project
+
+import * as React from 'react';
+
+import AppDrawer from './components/AppDrawer/AppDrawer';
+
+
+function App() {
+  return (
+    <AppDrawer />
+  );
+}
+
+
+export default App;
